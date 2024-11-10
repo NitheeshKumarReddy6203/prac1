8ac5d66329f09c45627e862e8461ee4ff213b6fe
Nitheesh Kumar Reddy <nitheeshkumarreddy5177@gmail.com>
2024-11-10 18:42:27 +0530

    Print.py file us Added!

---
+++ print.py
@@ -0,0 +1,7 @@
+print("Here! Now we are printing the Nums from 0 to 10")
+
+for i in range(0,11):
+    print(f"Iteration : {(i+1)} || Number : {i}")
+
+print("Here! we are done printing the numbers from 0 to 10")
+
